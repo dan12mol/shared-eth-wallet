c916b52a3753d5ed51a95c52930890338c884c04
Dan Moldovan <dan@TechQuarters-MacBook-Pro.local>
2021-03-22 22:22:42 +0100

    feat: add money received/sent events

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -37,6 +37,9 @@ contract Allowance is Ownable {
 }
 
 contract SimpleWallet is Allowance {
+    event MoneySent(address indexed _beneficiary, uint256 _amount);
+    event MoneyReceived(address indexed _from, uint256 _amount);
+
     function withdrawMoney(address payable _to, uint256 _amount)
         public
         ownerOrAllowed(_amount)
@@ -48,10 +51,13 @@ contract SimpleWallet is Allowance {
         if (owner() != msg.sender) {
             reduceAllowance(msg.sender, _amount);
         }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
 
-    receive() external payable {}
+    receive() external payable {
+        emit MoneyReceived(msg.sender, msg.value);
+    }
 
     fallback() external {}
 }
