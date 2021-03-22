674af628fb03f9b347e5df5829c27ccbb6ef6450
Dan Moldovan <dan@TechQuarters-MacBook-Pro.local>
2021-03-22 22:19:37 +0100

    feat: add allowance events

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -2,10 +2,18 @@ pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract SimpleWallet is Ownable {
+contract Allowance is Ownable {
+    event AllowanceChanged(
+        address indexed _forWho,
+        address indexed _fromWhom,
+        uint256 _oldAmount,
+        uint256 _newAmount
+    );
+
     mapping(address => uint256) public allowance;
 
     function addAllowance(address _who, uint256 _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
     }
 
@@ -18,9 +26,17 @@ contract SimpleWallet is Ownable {
     }
 
     function reduceAllowance(address _who, uint256 _amount) internal {
+        emit AllowanceChanged(
+            _who,
+            msg.sender,
+            allowance[_who],
+            allowance[_who] - _amount
+        );
         allowance[_who] -= _amount;
     }
+}
 
+contract SimpleWallet is Allowance {
     function withdrawMoney(address payable _to, uint256 _amount)
         public
         ownerOrAllowed(_amount)
