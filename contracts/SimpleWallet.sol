552c9405ea0dc2fc1a2aeab6f749e56313ca3669
Dan Moldovan <dan@TechQuarters-MacBook-Pro.local>
2021-03-22 22:39:50 +0100

    feat: split into multiple files

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -1,43 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
-
-contract Allowance is Ownable {
-    using SafeMath for uint256;
-
-    event AllowanceChanged(
-        address indexed _forWho,
-        address indexed _fromWhom,
-        uint256 _oldAmount,
-        uint256 _newAmount
-    );
-
-    mapping(address => uint256) public allowance;
-
-    function addAllowance(address _who, uint256 _amount) public onlyOwner {
-        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
-        allowance[_who] = _amount;
-    }
-
-    modifier ownerOrAllowed(uint256 _amount) {
-        require(
-            owner() == msg.sender || allowance[msg.sender] >= _amount,
-            "You are not allowed"
-        );
-        _;
-    }
-
-    function reduceAllowance(address _who, uint256 _amount) internal {
-        emit AllowanceChanged(
-            _who,
-            msg.sender,
-            allowance[_who],
-            allowance[_who].sub(_amount)
-        );
-        allowance[_who] = allowance[_who].sub(_amount);
-    }
-}
+import "./Allowance.sol";
 
 contract SimpleWallet is Allowance {
     event MoneySent(address indexed _beneficiary, uint256 _amount);
