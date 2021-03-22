1da36e8d1ec9a1e4c31e9adafb5fe1813311904e
Dan Moldovan <dan@TechQuarters-MacBook-Pro.local>
2021-03-22 19:36:11 +0100

    feat: add allowance state

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -3,9 +3,23 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract SimpleWallet is Ownable {
+    mapping(address => uint256) public allowance;
+
+    function addAllowance(address _who, uint256 _amount) public onlyOwner {
+        allowance[_who] = _amount;
+    }
+
+    modifier ownerOrAllowed(uint256 _amount) {
+        require(
+            owner() == msg.sender || allowance[msg.sender] >= _amount,
+            "You are not allowed"
+        );
+        _;
+    }
+
     function withdrawMoney(address payable _to, uint256 _amount)
         public
-        onlyOwner
+        ownerOrAllowed(_amount)
     {
         _to.transfer(_amount);
     }
