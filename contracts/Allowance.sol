4d1055f35e3ada8aca24a2d74be6b0405dc5526c
Dan Moldovan <dan@TechQuarters-MacBook-Pro.local>
2021-03-24 12:28:11 +0100

    chore: reindent

--- contracts/Allowance.sol
+++ contracts/Allowance.sol
@@ -4,37 +4,37 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
 contract Allowance is Ownable {
-    using SafeMath for uint256;
+  using SafeMath for uint256;
 
-    event AllowanceChanged(
-        address indexed _forWho,
-        address indexed _fromWhom,
-        uint256 _oldAmount,
-        uint256 _newAmount
+  event AllowanceChanged(
+    address indexed _forWho,
+    address indexed _fromWhom,
+    uint256 _oldAmount,
+    uint256 _newAmount
+  );
+
+  mapping(address => uint256) public allowance;
+
+  function addAllowance(address _who, uint256 _amount) public onlyOwner {
+    emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+    allowance[_who] = _amount;
+  }
+
+  modifier ownerOrAllowed(uint256 _amount) {
+    require(
+      owner() == msg.sender || allowance[msg.sender] >= _amount,
+      "You are not allowed"
     );
+    _;
+  }
 
-    mapping(address => uint256) public allowance;
-
-    function addAllowance(address _who, uint256 _amount) public onlyOwner {
-        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
-        allowance[_who] = _amount;
-    }
-
-    modifier ownerOrAllowed(uint256 _amount) {
-        require(
-            owner() == msg.sender || allowance[msg.sender] >= _amount,
-            "You are not allowed"
-        );
-        _;
-    }
-
-    function reduceAllowance(address _who, uint256 _amount) internal {
-        emit AllowanceChanged(
-            _who,
-            msg.sender,
-            allowance[_who],
-            allowance[_who].sub(_amount)
-        );
-        allowance[_who] = allowance[_who].sub(_amount);
-    }
+  function reduceAllowance(address _who, uint256 _amount) internal {
+    emit AllowanceChanged(
+      _who,
+      msg.sender,
+      allowance[_who],
+      allowance[_who].sub(_amount)
+    );
+    allowance[_who] = allowance[_who].sub(_amount);
+  }
 }
--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -3,31 +3,31 @@ pragma solidity ^0.8.0;
 import "./Allowance.sol";
 
 contract SimpleWallet is Allowance {
-    event MoneySent(address indexed _beneficiary, uint256 _amount);
-    event MoneyReceived(address indexed _from, uint256 _amount);
+  event MoneySent(address indexed _beneficiary, uint256 _amount);
+  event MoneyReceived(address indexed _from, uint256 _amount);
 
-    function withdrawMoney(address payable _to, uint256 _amount)
-        public
-        ownerOrAllowed(_amount)
-    {
-        require(
-            _amount <= address(this).balance,
-            "There are not enough funds stored in the smart contract"
-        );
-        if (owner() != msg.sender) {
-            reduceAllowance(msg.sender, _amount);
-        }
-        emit MoneySent(_to, _amount);
-        _to.transfer(_amount);
+  function withdrawMoney(address payable _to, uint256 _amount)
+  public
+  ownerOrAllowed(_amount)
+  {
+    require(
+      _amount <= address(this).balance,
+      "There are not enough funds stored in the smart contract"
+    );
+    if (owner() != msg.sender) {
+      reduceAllowance(msg.sender, _amount);
     }
+    emit MoneySent(_to, _amount);
+    _to.transfer(_amount);
+  }
 
-    function renounceOwnership() public view override onlyOwner {
-        revert("Can't renounce ownership");
-    }
+  function renounceOwnership() public view override onlyOwner {
+    revert("Can't renounce ownership");
+  }
 
-    receive() external payable {
-        emit MoneyReceived(msg.sender, msg.value);
-    }
+  receive() external payable {
+    emit MoneyReceived(msg.sender, msg.value);
+  }
 
-    fallback() external {}
+  fallback() external {}
 }
